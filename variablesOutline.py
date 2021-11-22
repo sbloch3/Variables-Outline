cd438b7ad2ec19bbff57e9630b3893052a42afa1
sbloch3 <sbloch3@wisc.edu>
2021-11-21 18:07:16 -0600

    Create variablesOutline.py

---
+++ variablesOutline.py
@@ -0,0 +1,119 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list):
+#Integer
+a = 5
+print (a)
+a = 4
+print (a)
+a= 3
+print (a)
+#Float
+
+a = 3.45
+
+a = 5.43
+
+a= 4.32
+
+#String
+
+a = "Samantha"
+
+a= "Michael"
+
+a= "Nino"
+
+a = "Samantha and Michael"
+
+#Booleans
+
+a = True
+
+a = False
+
+#List
+
+a = [1, 2,3, 4]
+
+a = ["Sam", "Michael", "Nino"]
+
+a = [True, False, True]
+
+a = [3.2, 4.5, 5.8]
+print(a)
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+
+varA = 1
+vara = 2
+
+var = 4
+var = "string"
+
+varTwo = var
+print(varTwo)
+
+b = [a, varA, var]
+print(b)
+
+list = ["a", "b", "c"]
+print(list[2])
+
+listVar = list[0]
+print(listVar)
+
+string = "Samantha"
+print(string[0])
+
+print(5)
+print("string")
+print(True)
+print(list)
+
+
+'''Syntax Error and Logical Errors '''
+
+var =4
+print(var)
+
+
+
+
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+
+
+#4)
+#Make a variable and set it to a variable you have already created:
+
+
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+
+
+
+#6)
+#Make a String set to your name. Make a variable that is set to the first
+#character in that string:
+
+
+
+#7)
+#Print out ONE of any of the variables you have created so far:
+
+
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+
+
